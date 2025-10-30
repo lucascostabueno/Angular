eaa77f3c005103734582600f02f48543ac3c5cc4
lucascostabueno <lucasbueno722g@gmail.com>
2025-10-30 09:24:31 -0400

    secao 2 / aula 29: Precisamos de Componentes Mais Flexíveis

--- maximilian/secao2/01-starting-project/src/app/user/user.component.ts
+++ maximilian/secao2/01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,26 +9,5 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // signals
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  /*  zone.js
-  selectedUser = DUMMY_USERS[randomIndex]; */
-
-  // signals
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  /*   zone.js
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  } */
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // signals
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-
-    /* zone.js
-    this.selectedUser = DUMMY_USERS[randomIndex]; */
-  }
+  onSelectUser() {}
 }
