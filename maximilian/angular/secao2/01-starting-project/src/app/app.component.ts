0d95260e0b11c4051e2d9e4a2d2e2461afc6a854
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-05 09:46:03 -0400

    secao 2 / aula 44: Adicionando Mais Componentes ao Aplicativo de Demonstração

--- maximilian/angular/secao2/01-starting-project/src/app/app.component.ts
+++ maximilian/angular/secao2/01-starting-project/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
@@ -8,7 +7,7 @@ import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent, HeaderComponent, TasksComponent, NgFor, NgIf],
+  imports: [UserComponent, HeaderComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
