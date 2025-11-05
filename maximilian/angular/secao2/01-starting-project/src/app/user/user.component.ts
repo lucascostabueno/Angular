bb5e9bf6bc4423ca9b424f23e6a6904dcadaf243
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-05 16:27:02 -0400

    secao 2 / aula 47: Storing Data Models in Separate Files

--- maximilian/angular/secao2/01-starting-project/src/app/user/user.component.ts
+++ maximilian/angular/secao2/01-starting-project/src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-/* type User = {
-  id: string;
-  avatar: string;
-  name: string;
-}; */
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -19,11 +9,6 @@ interface User {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) user!: {
-  //   id: string;
-  //   avatar: string;
-  //   name: string;
-  // };
   @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
